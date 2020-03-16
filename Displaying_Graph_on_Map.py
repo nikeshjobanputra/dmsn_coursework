15d59fd5d96ffab349d0f523719c421e3ebe528b
Nikesh Jobanputra <nikesh.jobanputra1@gmail.com>
2020-03-16 15:21:36 +0000

    Implemented python script to map nodes and edges on map

---
+++ Displaying_Graph_on_Map.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+os.environ["PROJ_LIB"] = "C:\\Users\\Nikesh\\Anaconda3\\pkgs\\proj4-5.2.0-ha925a31_1\\Library\\share"
+from mpl_toolkits.basemap import Basemap as Basemap
+
+flight_data_col = ['Source', 'Destination']
+flight_data_df = pd.read_csv('flight_data.csv', names=flight_data_col, skiprows=[0])
+flight_data_df['Source'] = flight_data_df['Source'].astype("string")
+flight_data_df['Destination'] = flight_data_df['Destination'].astype("string")
+
+nodes_lat_long_col = ['Node', 'Airport Name', 'Lon', 'Lat']
+nodes_lat_long_df = pd.read_csv('nodes_lat_long.csv', names=nodes_lat_long_col, skiprows=[0])
+nodes_lat_long_df['Node'] = nodes_lat_long_df['Node'].astype("string")
+nodes_lat_long_df['Airport Name'] = nodes_lat_long_df['Airport Name'].astype("string")
+
+graph = nx.from_pandas_edgelist(flight_data_df, source = 'Source', target = 'Destination' ,create_using = nx.DiGraph())
+
+plt.figure(figsize = (10,9))
+m = Basemap(projection='merc',llcrnrlon=-180,llcrnrlat=10,urcrnrlon=-50,urcrnrlat=70, lat_ts=0, resolution='l',suppress_ticks=True)
+
+mx, my = m(nodes_lat_long_df['Lon'].values, nodes_lat_long_df['Lat'].values)
+pos = {}
+for count, elem in enumerate (nodes_lat_long_df['Node']):
+     pos[elem] = (mx[count], my[count])
+
+nx.draw_networkx_nodes(G = graph, pos = pos, node_list = graph.nodes(),node_color = 'r', alpha = 0.8, node_size = 100)
+nx.draw_networkx_edges(G = graph, pos = pos, edge_color='g', alpha=0.2, arrows = False)
+
+m.drawcountries(linewidth = 3)
+m.drawstates(linewidth = 0.2)
+m.drawcoastlines(linewidth=3)
+plt.tight_layout()
+plt.savefig("map_1.png", format = "png", dpi = 300)
--- SEIR_Model_Implementation.py
+++ SEIR_Model_Implementation.py
@@ -1,10 +1,28 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import ndlib.models.ModelConfig as mc
+import ndlib.models.epidemics as ep
+from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
 
 data = open('edgesList.csv', 'r')
 graphType = nx.DiGraph()
 
 g = nx.parse_edgelist(data, delimiter=',', create_using=graphType,
                       nodetype=str)
-nx.draw(g)
-plt.savefig("filename.png")
+
+
+model = ep.SEIRModel(g)
+
+cfg = mc.Configuration()
+cfg.add_model_parameter('beta', 0.01)
+cfg.add_model_parameter('gamma', 0.005)
+cfg.add_model_parameter('alpha', 0.05)
+cfg.add_model_parameter("fraction_infected", 0.05)
+model.set_initial_status(cfg)
+
+iterations = model.iteration_bunch(200)
+
+trends = model.build_trends(iterations)
+
+viz = DiffusionTrend(model, trends)
+viz.plot("diffusion.pdf")
