b95f71e10e4db4df4f6353e2863262f960b68fad
Nikesh Jobanputra <nikesh.jobanputra1@gmail.com>
2020-03-16 15:26:58 +0000

    Changed to SIS model

--- SIS_Model_Implementation.py
+++ SIS_Model_Implementation.py
@@ -4,25 +4,26 @@ import ndlib.models.ModelConfig as mc
 import ndlib.models.epidemics as ep
 from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
 
-data = open('edgesList.csv', 'r')
+data = open('flight_data.csv', 'r')
 graphType = nx.DiGraph()
 
 g = nx.parse_edgelist(data, delimiter=',', create_using=graphType,
                       nodetype=str)
 
 
-model = ep.SEIRModel(g)
+model = ep.SISModel(g)
 
+#WE NEED TO FIGURE THE PARAMS OUT BELOW
 cfg = mc.Configuration()
 cfg.add_model_parameter('beta', 0.01)
-cfg.add_model_parameter('gamma', 0.005)
-cfg.add_model_parameter('alpha', 0.05)
+cfg.add_model_parameter('lambda', 0.005)
 cfg.add_model_parameter("fraction_infected", 0.05)
 model.set_initial_status(cfg)
+
 
 iterations = model.iteration_bunch(200)
 
 trends = model.build_trends(iterations)
 
 viz = DiffusionTrend(model, trends)
-viz.plot("diffusion.pdf")
+viz.plot("diffusion.png")
