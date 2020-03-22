5ef4c3d250a4869301b93b8013ce856e5c77fbe6
Nikesh Jobanputra <nikesh.jobanputra1@gmail.com>
2020-03-22 17:22:18 +0000

    Updating script to remove edges

--- Remove_Edges_From_LAX.py
+++ Remove_Edges_From_LAX.py
@@ -13,17 +13,22 @@ threshold=0.4
 name_of_new_csv = "fourty_percentage_threshold.csv"
 
 removed_edges_from_LAX=0
+count_of_LAX_edges=0
 
 for index, row in flight_data_df.iterrows():
-    if(random.random()<=threshold):
-        if row['Source'] == 'LAX':
+    if row['Source'] == 'LAX':
+        count_of_LAX_edges+=1
+        if(random.random()<=threshold):
             removed_edges_from_LAX+=1
             flight_data_df.drop(index, inplace=True)
-        elif row['Destination'] == 'LAX':
+    elif row['Destination'] == 'LAX':
+        count_of_LAX_edges+=1
+        if(random.random()<=threshold):
             removed_edges_from_LAX+=1
             flight_data_df.drop(index, inplace=True)
 
-
+print("Total amount of edges to and from LAX is ", count_of_LAX_edges)
 print("Amount of removed edges to and from LAX is ", removed_edges_from_LAX)
+print("Percentage removed is ", (removed_edges_from_LAX/count_of_LAX_edges)*100, "%")
 
 flight_data_df.to_csv(name_of_new_csv, index=None, header=True)
